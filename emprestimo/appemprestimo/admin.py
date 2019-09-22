22cff8f40d83742e7d9ab21d696350241d5f3e2f
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-22 13:47:32 -0300

    Modificado os arquivos admin.py, settings.py, models.py

--- emprestimo/appemprestimo/admin.py
+++ emprestimo/appemprestimo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration', 'date_birth')
+
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('date_devolution', 'date_withdrawn', 'student','returned')
+    filter_horizontal = ['books']
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name')
+
+
+admin.site.register(Livro)
+admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Autor,AutorAdmin)
+admin.site.register(Emprestimo,EmprestimoAdmin)
--- emprestimo/appemprestimo/models.py
+++ emprestimo/appemprestimo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Autor(models.Model):
     class Meta:
-        nome_plural = "autores"
+        verbose_name_plural = "autores"
     
     name = models.CharField("nome",max_length=100)
     last_name = models.CharField("sobrenome", max_length=100)
@@ -13,18 +13,17 @@ class Autor(models.Model):
 
 
 class Aluno(models.Model):
+    class Meta:
+        verbose_name_plural = "alunos"
     SEXO_CHOICES = [
         ('MAS', 'Masculino'),
         ('FEM', 'Feminino'),
         ('OUT', 'Outros'),
     ]
-    class Meta:
-        nome_plural = "alunos"
-    
     name = models.CharField("nome", max_length=100)
     last_name = models.CharField("sobrenome", max_length=100)
-    registration = models.CharField("matricula", max_length=10)
-    sex = models.CharField("sexo", choices=SEXO_CHOICES, default="Masculino")
+    registration = models.CharField("matricula", max_length=10, unique=True)
+    sex = models.CharField("sexo", choices=SEXO_CHOICES, default="Masculino", max_length=3)
     date_birth = models.DateField("data_nasc")
     email = models.EmailField("e-mail")
 
@@ -32,25 +31,25 @@ class Aluno(models.Model):
     def __str__(self):
         return self.last_name.upper() + ',' + self.name
 
-    class Livro(models.Model):
-        class Meta:
-            nome_plural = "livros"
-        title = models.CharField("titulo", max_length=50)
-        author = models.ForeignKey("foreignkey_autor", Autor, on_delete=models.CASCADE)
-        year_publication = models.IntegerField("ano_publicacao")
+class Livro(models.Model):
+    class Meta:
+         verbose_name_plural = "livros"
+    title = models.CharField("titulo", max_length=50)
+    author = models.ForeignKey( Autor, on_delete=models.CASCADE)
+    year_publication = models.IntegerField("ano_publicacao")
 
-        def __str__(self):
-            return "{}, ({}) ".format(self.title, self.year_publication)
+    def __str__(self):
+        return "{}, ({}) ".format(self.title, self.year_publication)
 
 
 
-    class Emprestimo(models.Model):
-        class Meta:
-            nome_plural = "emprestimos"
-        user = models.ForeignKey("foreignkey_usuario", 'auth.User', on_delete=models.CASCADE)
-        student = models.ForeignKey("foreignkey_aluno", Aluno, on_delete=models.CASCADE)
-        date_withdrawn = models.DateField("data_retirada")
-        date_devolution = models.DateField("data_devolucao")
-        books = models.ForeignKey("livros", Livro, on_delete=models.CASCADE)
-        returned = models.BooleanField("devolvido")
+class Emprestimo(models.Model):
+    class Meta:
+        verbose_name_plural = "emprestimos"
+    user = models.ForeignKey( 'auth.User', on_delete=models.CASCADE)
+    student = models.ForeignKey( Aluno, on_delete=models.CASCADE)
+    date_withdrawn = models.DateField("data_retirada")
+    date_devolution = models.DateField("data_devolucao")
+    books = models.ManyToManyField(Livro)
+    returned = models.BooleanField("devolvido")
 
